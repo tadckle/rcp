f58ab6ec043dbaafe4431088041aa0a9474e0778
tadckle <tadckle@126.com>
2018-07-02 13:02:03 +0800

    Refine code

--- rcp3/rcp3.study/src/rcp3/study/viewers/ViewerComparatorAllInOne.java
+++ rcp3/rcp3.study/src/rcp3/study/viewers/ViewerComparatorAllInOne.java
@@ -14,10 +14,12 @@ import org.eclipse.jface.viewers.ViewerComparator;
 import org.eclipse.swt.SWT;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.primitives.Doubles;
+
 import rcp3.study.common.AlphanumComparator;
 
 /**
- * The abstract comparator for viewer.
+ * The comparator for TableViewer, TreeViewer, GridTableViewer and GridTreeViewer.
  *
  * @author alzhang
  */
@@ -30,6 +32,17 @@ public class ViewerComparatorAllInOne extends ViewerComparator {
     super(new AlphanumComparator());
   }
 
+  /**
+   * By default all element has same category value.
+   *
+   * @param element a row element.
+   * @param sortDir the sort direction.
+   * @return category value.
+   */
+  public int category(Object element, int sortDir) {
+    return 0;
+  }
+
   @Override
   public int compare(Viewer viewer, Object element1, Object element2) {
     if (!(viewer instanceof ColumnViewer)) {
@@ -37,15 +50,29 @@ public class ViewerComparatorAllInOne extends ViewerComparator {
     }
 
     ViewerSortFactor sortFactor = ViewerSortFactor.get(viewer);
-    String text1 = getElementText((ColumnViewer) viewer, element1, sortFactor.getColumnIndex());
-    String text2 = getElementText((ColumnViewer) viewer, element2, sortFactor.getColumnIndex());
-
     if (sortFactor.getColumnIndex() < 0 || sortFactor.getSortDir() == SWT.NONE) {
       return 0;
     }
 
-    int result = getComparator().compare(text1, text2);
-    return SWT.UP == sortFactor.getSortDir() ? result : -result;
+    int cat1 = category(element1);
+    int cat2 = category(element2);
+    if (cat1 == cat2) {
+      cat1 = category(element1, sortFactor.getSortDir());
+      cat2 = category(element2, sortFactor.getSortDir());
+    }
+
+    if (cat1 != cat2) {
+      return SWT.DOWN == sortFactor.getSortDir() ? cat1 - cat2 : cat2 - cat1;
+    }
+
+    String text1 = getElementText((ColumnViewer) viewer, element1, sortFactor.getColumnIndex());
+    String text2 = getElementText((ColumnViewer) viewer, element2, sortFactor.getColumnIndex());
+    Double value1 = Doubles.tryParse(text1);
+    Double value2 = Doubles.tryParse(text2);
+
+    int result = value1 != null && value2 != null ? Double.compare(value1, value2)
+        : ((AlphanumComparator) getComparator()).compare(text1, text2);
+    return SWT.DOWN == sortFactor.getSortDir() ? result : -result;
   }
 
   private String getElementText(ColumnViewer viewer, Object element, int columnIndex) {
