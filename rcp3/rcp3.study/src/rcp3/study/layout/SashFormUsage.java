8dffbc3df66ca4985e8641f43eed7360a52e4660
Alex <tadckle@126.com>
2019-06-26 20:23:05 +0800

    Update SashFormUsage.java

--- rcp3/rcp3.study/src/rcp3/study/layout/SashFormUsage.java
+++ rcp3/rcp3.study/src/rcp3/study/layout/SashFormUsage.java
@@ -2,13 +2,8 @@ package rcp3.study.layout;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Sash;
 
 import rcp3.study.ShellRunner;
 
@@ -28,39 +23,8 @@ public class SashFormUsage implements ShellRunner {
     Button btn2 = new Button(sashForm, SWT.PUSH);
     btn2.setText("Button 2");
 
-    Listener listener = new Listener() {
-      @Override
-      public void handleEvent(Event event) {
-        hackSashForm(sashForm, 150, 250);
-        btn1.removeListener(SWT.Resize, this);
-      }
-    };
-    btn1.addListener(SWT.Resize, listener);
+    SashFormUtil.setMinimumSize(sashForm, 200, 400);
   }
 
-  private void hackSashForm(SashForm sashForm, int leftWidth, int rightWidth) {
-    Control[] comps = sashForm.getChildren();
-    for (Control comp : comps){
-
-      if (comp instanceof Sash){
-        final int SASH_LIMIT = 200;
-        final Sash sash = (Sash)comp;
-
-        Listener listener = sash.getListeners(SWT.Selection)[0];
-        sash.removeListener(SWT.Selection, listener);
-        sash.addListener(SWT.Selection, new Listener () {
-          @Override
-          public void handleEvent(Event event) {
-            Rectangle rect = sash.getParent().getClientArea();
-            if (Double.compare(event.x, leftWidth) < 0 || Double.compare(event.x, rect.width - rightWidth) > 0) {
-              event.doit = false;
-            } else {
-              listener.handleEvent(event);
-            }
-          }
-        });
-      }
-    }
-  }
 
 }
